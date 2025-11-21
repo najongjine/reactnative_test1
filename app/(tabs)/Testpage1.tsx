9327c2a7420066fab2ff2c745b546b04f097b60c
najongjine <najongjin3@hotmail.com>
2025-11-21 10:41:53 +0900

    .

---
+++ app/(tabs)/Testpage1.tsx
@@ -0,0 +1,30 @@
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+
+export default function Testpage1() {
+  return (
+    <ScrollView>
+      <View>
+        <Text>test1 페이지 에요</Text>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
